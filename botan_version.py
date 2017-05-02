d1cde531838b8465185c6283966e721b828a9bbb
Vital Teresh <vster2006@gmail.com>
2017-05-02 16:03:43 +0300

    Botan-1.10.9 first commit

---
+++ botan_version.py
@@ -0,0 +1,11 @@
+
+release_major = 1
+release_minor = 10
+release_patch = 9
+
+release_so_abi_rev = 0
+
+# These are set by the distribution script
+release_vc_rev = 'mtn:052e8b9f16011d0ef24217fa47caffb6e717ac95'
+release_datestamp = 20141213
+release_type = 'released'
